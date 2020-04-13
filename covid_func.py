e15f549d5eda55d3687bfd7582a3e61136329d16
Inoueian <satoruphys@gmail.com>
2020-04-12 22:43:33 -0500

    Add functions to create distribution functions

--- covid_func.py
+++ covid_func.py
@@ -1,2 +1,17 @@
 import numpy as np
+from scipy.stats.distributions import gamma, lognorm
 
+
+def lognorm_pdf(mean, sd):
+    """Define a log-normal PDF from its mean and standard deviation"""
+    sigma = np.sqrt(np.log(1 + sd**2 / mean**2))
+    mu = np.log(mean) - (sigma**2 / 2)
+    return lambda x: lognorm.pdf(x, s=sigma, scale=np.exp(mu))
+
+
+def gamma_pdf(mean, sd):
+    """Define a gamma distribution PDF from its mean and standard deviation"""
+    coef_of_variation = sd / mean
+    alpha = 1 / coef_of_variation**2
+    beta = alpha / mean
+    return lambda x: gamma.pds(x, a=alpha, scale=1/beta)
